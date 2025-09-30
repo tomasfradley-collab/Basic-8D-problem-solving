b29b175667be6916fbcdf2516e1e137c8d862866
tomasfradley-collab <tomas.fradley@gmail.com>
2025-09-30 12:36:47 +0100

    fix: Update asset paths for PWA compatibility
    Changes relative paths for static assets to ensure correct loading in the Service Worker and manifest, improving PWA functionality.

--- index.tsx
+++ index.tsx
@@ -17,7 +17,7 @@ root.render(
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
+    navigator.serviceWorker.register('./service-worker.js')
       .then(registration => {
         console.log('ServiceWorker registration successful with scope: ', registration.scope);
       })
